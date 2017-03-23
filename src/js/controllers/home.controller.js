74aacbce40de909e2f2f07286df071367f17d846
Dmitry Bakhtin <unlimiterider@gmail.com>
2017-03-23 12:01:18 +0000

    0.0.2

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -5,12 +5,24 @@
 			.module('reviewApp')
 			.controller('homeController', homeController);
 
-	homeController.$inject = ['$scope', 'dataService', '$state'];
+	homeController.$inject = ['$scope', 'dataService', '$state', '$rootScope'];
 
-	function homeController($scope, dataService, $route){
+	function homeController($scope, dataService, $state, $rootScope){
 		console.log('homeController loaded');
 		var vm = this;
+		console.log($state.current);
+		vm.state = $state.current.name;
+		vm.shareDisabled = true;
+		vm.sendDisabled = true;
+		vm.dealButtons = function() {
+			if (vm.state === 'send') {
+				console.log('state SEND');
+			} else if (vm.state === 'share') {
+				console.log('state share');
+			}
+		};
 
+		$rootScope.$on('$stateChangeSuccess', vm.dealButtons);
 	}
 
 })();
